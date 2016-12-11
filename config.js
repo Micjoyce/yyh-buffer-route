07582776da915824a3a8813a7c2c6f248cb6555c
michael_xu <michael_xu@caifujilu.com>
2016-12-11 15:51:05 +0800

    add

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-	bufferName: "Buffer1",
+	bufferName: "Buffer1",  // 修改buffer点的名称 Buffer1， Buffer2
 	combinFlag: "-",
-	annealDegree: 300,
-	annealFactor: 0.98,
+	annealDegree: 300,		 // 起始退火温度
+	annealFactor: 0.98,    //
 	iteratorTimes: 80,
 	stopTime: 400,
-	digital: 2
+	digital: 2   // 不用管
 }
