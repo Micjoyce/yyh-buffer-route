3cc5eb5bad34957e2ae4dd8ada95821810e618df
michael_xu <michael_xu@caifujilu.com>
2016-12-11 15:08:33 +0800

    change iterator

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	bufferName: "Buffer1",
 	combinFlag: "-",
-	annealDegree: 260,
-	annealFactor: 0.8,
-	iteratorTimes: 50,
-	stopTime: 7,
+	annealDegree: 300,
+	annealFactor: 0.98,
+	iteratorTimes: 80,
+	stopTime: 400,
 	digital: 2
 }
